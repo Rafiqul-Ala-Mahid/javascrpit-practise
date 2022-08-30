87375a103dfdb75e5718eac66d32b2fb6bc6b115
MacBook <macbook@MacBooks-Air.Dlink>
2022-08-30 20:03:46 +0600

    ninth commit

--- select.js
+++ select.js
@@ -2,75 +2,69 @@ function select(id) {
     count++
     if (count < 6) {
         const div = document.querySelector('.era')
-        const name = document.createElement('ol')
+        const name = document.createElement('ul')
         div.appendChild(name)
         const add = document.createElement('li')
         add.innerText = count + '. ' + document.getElementById(id).parentNode.childNodes[3].innerText;
         name.appendChild(add)
     }
+    else {
+        alert("Sorry! You can't select more.")
+        count--
+        a=false
+    }
 }
-
+let a=true
 let count=0
 
-document.getElementById("mybtn1").addEventListener("click", myFunction1);
-
-function myFunction1() {
+document.getElementById("mybtn1").addEventListener("click", function () {
     select('mybtn1')
-    if (count < 6) {
+    if (count < 6 && a) {
         document.querySelector('#mybtn1').disabled = true
         mybtn1.style.backgroundColor = 'lightblue'
     }
-}
+});
 
-document.getElementById("mybtn2").addEventListener("click", myFunction2);
-
-function myFunction2() {
+document.getElementById("mybtn2").addEventListener("click", function () {
     select('mybtn2')
-    if (count < 6) {
+    if (count < 6 && a) {
         document.querySelector('#mybtn2').disabled = true
         mybtn2.style.backgroundColor = 'lightblue'
     }
-}
+});
 
-document.getElementById("mybtn3").addEventListener("click", myFunction3);
-
-function myFunction3() {
+document.getElementById("mybtn3").addEventListener("click", function () {
     select('mybtn3')
-    if (count < 6) {
+    if (count < 6 && a) {
         document.querySelector('#mybtn3').disabled = true
         mybtn3.style.backgroundColor = 'lightblue'
     }
-}
+});
 
-document.getElementById("mybtn4").addEventListener("click", myFunction4);
 
-function myFunction4() {
+document.getElementById("mybtn4").addEventListener("click", function () {
     select('mybtn4')
-    if (count < 6) {
+    if (count < 6 && a) {
         document.querySelector('#mybtn4').disabled = true
         mybtn4.style.backgroundColor = 'lightblue'
     }
-}
+});
 
-document.getElementById("mybtn5").addEventListener("click", myFunction5);
-
-function myFunction5() {
+document.getElementById("mybtn5").addEventListener("click", function () {
     select('mybtn5')
-    if (count < 6) {
+    if (count < 6 && a) {
         document.querySelector('#mybtn5').disabled = true
         mybtn5.style.backgroundColor = 'lightblue'
     }
-}
+});
 
-document.getElementById("mybtn6").addEventListener("click", myFunction6);
-
-function myFunction6() {
+document.getElementById("mybtn6").addEventListener("click", function () {
     select('mybtn6')
-    if (count < 6) {
+    if (count < 6 && a) {
         document.querySelector('#mybtn6').disabled = true
         mybtn6.style.backgroundColor = 'lightblue'
     }
-}
+});
 
 document.getElementById('cost-btn').addEventListener("click", function () {
     const playerInputString = document.getElementById('per-cost').value
